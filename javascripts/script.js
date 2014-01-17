68222b46ab28a1e1e4a87ca4ee9b00be4ba5d7a2
domurtag <domurtag@yahoo.co.uk>
2014-01-17 15:18:05 +0000

    added Google Analytics JS and changed conference banner

--- javascripts/script.js
+++ javascripts/script.js
@@ -307,4 +307,19 @@ function createTwitter() {
         });
     });
 }
-;
+
+// Google analytics
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-47269513-1', 'ic-aviation.com');
+ga('send', 'pageview');
